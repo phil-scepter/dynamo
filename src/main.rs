82f60d42f968b297320f7733edbe699e892599fa
Phil Opaola <phillips.opaola@gmail.com>
2022-05-24 15:19:22 +0200

    gossip protocol end to end functional

--- src/main.rs
+++ src/main.rs
@@ -10,11 +10,11 @@ next steps:
 use std::collections::{HashMap, HashSet};
 use std::collections::hash_map::Iter;
 use std::hash::Hash;
-use std::io::{Read, Write};
+use std::io::{BufRead, BufReader, BufWriter, Read, Write};
 use std::net::{IpAddr, SocketAddr, SocketAddrV4, TcpListener, TcpStream, ToSocketAddrs};
 use std::sync::{Arc, RwLock};
-use std::{env, thread};
-use std::fmt::Error;
+use std::{env, io, thread};
+use std::fmt::{Debug, Error};
 use std::thread::sleep;
 use std::time::{Duration, Instant, SystemTime, UNIX_EPOCH};
 use crossbeam::channel::{after, bounded, tick};
@@ -26,6 +26,22 @@ fn remove_whitespace(s: &str) -> String {
 
 fn now() -> Duration {
     return SystemTime::now().duration_since(UNIX_EPOCH).unwrap_or_default();
+}
+
+struct BufTcpStream {
+    input: BufReader<TcpStream>,
+    unused: BufWriter<TcpStream>,
+    output: TcpStream,
+}
+
+impl BufTcpStream {
+    fn new(stream: TcpStream) -> io::Result<Self> {
+        let input = BufReader::new(stream.try_clone()?);
+        let unused = BufWriter::new(stream.try_clone()?);
+        let output = stream;
+
+        Ok(Self { input, unused, output })
+    }
 }
 
 pub struct PeerMap {
@@ -78,16 +94,40 @@ impl Clone for PeerMap {
 }
 
 trait WireMessage {
-    fn process(stream: &mut TcpStream); // -> std::result::Result<Self, Error>;
+    fn process(&self, stream: BufTcpStream); // -> std::result::Result<Self, Error>;
 }
 
-struct RequestPeers {}
+struct RequestPeers {
+    peer_map: PeerMap,
+}
+
+impl RequestPeers {
+    fn new(peer_map: PeerMap) -> Self {
+        return RequestPeers{peer_map};
+    }
+}
 
 impl WireMessage for RequestPeers {
-    fn process(stream: &mut TcpStream) {
-        let peers = "127.0.0.1:8001\n127.0.0.1:8002\n127.0.0.1:8003\n127.0.0.1:8004\n127.0.0.1:8005";
-        stream.write(peers.as_bytes()).expect("Failed to write to server");
+    fn process(&self, mut stream: BufTcpStream) {
+        println!("writing peers to stream");
+        let mut buffer = "".to_owned(); // ":127.0.0.1:8001\n127.0.0.1:8002\n127.0.0.1:8003\n127.0.0.1:8004\n127.0.0.1:8005\n";
+        for (peer_addr, _) in self.peer_map.pairs() {
+            buffer += &format!("{}\n", peer_addr);
+        }
 
+        println!("payload is {}", buffer);
+        let bytes_written = stream.unused.write(buffer.as_bytes()).unwrap(); // expect("Failed to write to server");
+
+        println!("wrote {} bytes to buffer", bytes_written);
+        match stream.output.flush(){
+            Ok(ok) => {
+                println!("success response from flush {:?}", ok);
+            },
+            Err(e) => {
+                println!("error response from flush {:?}", e);
+            }
+        };
+        println!("flushed result");
         return;
     }
 }
@@ -96,25 +136,73 @@ struct WireProtocol {
 }
 
 impl WireProtocol {
-    pub fn parse_request(stream: &mut TcpStream) {
-        let mut bytes: [u8; 128] = [0;128]; // todo - arbitrary sized read
-        let read_result = stream.read(&mut bytes);
-        let total_bytes_read = match read_result {
-            Ok(n) => { n }
-            Err(_) => { return; }
+    pub fn parse_request(mut stream: TcpStream, peer_map: PeerMap) {
+        let mut buf_stream = match BufTcpStream::new(stream) {
+            Ok(buf_stream) => {
+                println!("successfully created BufTcpStream");
+                buf_stream
+            },
+            Err(e) => {
+                println!("error creating BufTcpStream {}", e);
+                return;
+            },
         };
 
-        if total_bytes_read == 0 || bytes.len() < 14 { // number can change replace later with codified minimum message length
+        let mut command= String::new();
+        println!("about to read result");
+        let read_result = buf_stream.input.read_line(&mut command);
+        println!("read_result: {:?}", read_result);
+        let mut total_bytes_read = match read_result {
+            Ok(n) => {
+                println!("n: {}", n);
+                n
+            },
+            Err(e) => {
+                println!("error reading off buf_stream {}", e);
+                return;
+            },
+        };
+
+        println!("total_bytes_read: {}", total_bytes_read);
+
+        if total_bytes_read == 0 || command.len() < 13 { // number can change replace later with codified minimum message length
             return
         }
 
-        let buffer = std::str::from_utf8(&mut bytes).expect("could not parse message");
-        println!("received buffer {}", buffer);
-        match &buffer[0..13] {
-            "request_peers" => {
-                RequestPeers::process(stream)
+        let request_peers = "request_peers\n".to_string();
+
+        let mut peer_address = String::new();
+        total_bytes_read = buf_stream.input.read_line(&mut peer_address).unwrap();
+        peer_address = remove_whitespace(&peer_address);
+        println!("peer_address value is {}", peer_address);
+        println!("total_bytes_read for peer_address: {}", total_bytes_read);
+
+        let remote_addr = match peer_address.parse::<SocketAddr>(){
+            Ok(v) => {
+                println!("remote socket_addr is {}", v);
+                v
             },
-            _ => return,
+            Err(e) => {
+                println!("error coercing remote socket_addr from str : {}", e);
+                return
+            },
+        };
+        println!("remote_addr is {}", remote_addr);
+
+        if None == peer_map.get(&remote_addr) {
+            peer_map.set(remote_addr, now());
+        }
+
+        match command { // newline
+            request_peers=> {
+                let handler = RequestPeers::new(peer_map);
+                println!("created handler");
+                handler.process(buf_stream)
+            },
+            v => {
+                println!("unexpected value {}", v);
+                return;
+            },
         }
 
         return
@@ -141,8 +229,10 @@ impl Node {
             match stream {
                 Err(e) => { eprint!("failed {}", e) }
                 Ok(mut stream) => {
+                    let peer_map: PeerMap = self.peer_map.clone();
+
                     thread::spawn(move || { // thread pool
-                        WireProtocol::parse_request(&mut stream);
+                        WireProtocol::parse_request(stream, peer_map);
                     });
                 }
             }
@@ -189,21 +279,23 @@ impl Node {
         let mut new_peers = HashSet::new();
 
         if self.address == *peer_addr {
-            println!("early exit");
-            return None; // ret none probably redundant, either Result<Hashmap, Err> or just return an empty hashmap
+            return None;
         }
 
         let conn_result = TcpStream::connect(peer_addr);
         match conn_result {
             Ok(mut conn) => {
-                let resp = conn.write("request_peers".as_bytes());
+                let local_addr = self.address.to_string();
+                println!("local_addr is {}", local_addr);
+                let request = format!("request_peers\n{}\n", local_addr);
+                let resp = conn.write(request.as_bytes());
                 if resp.is_err() {
                     println!("Failed to send request to peer {}", peer_addr);
                     return None;
                 }
 
-                println!("wrote request_peers to peer {}", peer_addr);
-                // conn.set_read_timeout(Some(Duration::from_millis(5000)));
+                println!("wrote request_peers to peer {} with {}", peer_addr, request);
+                conn.set_read_timeout(Some(Duration::from_millis(5000)));
                 conn.read(buf);
                 println!("read response from peer {} is {:?}", peer_addr, buf);
             },
@@ -238,7 +330,7 @@ impl Node {
 }
 
 pub fn main(){
-    let ticker = tick(Duration::from_millis(500));
+    let ticker = tick(Duration::from_millis(2500));
     let (sender , requests) = bounded::<(SocketAddr, Duration)>(3);
 
     let hashmap: HashMap<SocketAddr, Duration> = HashMap::new();
@@ -272,7 +364,7 @@ pub fn main(){
     let args: Vec<String> = env::args().collect();
     let hostname = &args[1];
     let port = &args[2];
-    let seed_node_address = SocketAddr::V4("127.0.0.1:8080".parse().unwrap());
+    let seed_node_address = SocketAddr::V4("127.0.0.1:8002".parse().unwrap());
 
     let config = Config {
         seed_node_address: seed_node_address,
@@ -298,17 +390,7 @@ pub fn main(){
         address: SocketAddr::V4(format!("{}:{}", hostname, port).parse().unwrap()),
     };
 
-    thread::spawn(move || {
-        listener_node.listen();
-    });
-
-    let mut index = 1;
-    loop {
-        let ip_addr = format!("127.0.0.1:8080");
-        sender.send((SocketAddr::V4(ip_addr.parse().unwrap()), now()));
-        thread::sleep(Duration::from_secs(1));
-        index += 1;
-    }
+    listener_node.listen();
 }
 
 #[test]
@@ -353,6 +435,5 @@ fn test_peermap(){ // ensure that peermap provides a thread safe interface
         index += 1;
     }
     assert_eq!(peer_map.size(), 5);
-
     // TODO: test get interface
 }
